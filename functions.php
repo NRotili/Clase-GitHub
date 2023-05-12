390782aab829f8b542e9244ee01704694a436c3f
Emmanuel Angelozzi <emmanuelangelozzi@gmail.com>
2023-05-11 21:10:01 -0300

    Creat function hipotenuza issue#5

--- functions.php
+++ functions.php
@@ -77,3 +77,18 @@ function lista($num1){
     rsort($num1);
     return $num1[0];
 };
+ * Issue: #5
+ * Created by: @EmmanuelAngelozzi
+ * Review by: 
+ * Version: 1.0
+ */
+//Como operador necesito una función que calcule la hipotenusa (h2=n2+m2) de un triángulo rectángulo dados sus catetos.
+
+hipotenuza(7, 2);
+
+function hipotenuza($n, $m)
+{
+    $h = $m*2 + $n*2;
+    return sqrt($h);
+}
+echo hipotenuza(7,2);
